f552b6e6ff6dc83c3e084d8b8a4dd59baa2d14ef
jeffnumber1 <jefersonfilho440@gmail.com>
2024-10-17 21:17:26 -0300

    aula JS Ermeson

---
+++ JS/JS02-Console/main.js
@@ -0,0 +1,41 @@
+// let nota1, nota2, nota3, nota4, media;
+// nota1 = 7
+// nota2 = 5
+// nota3 = 2
+// nota4 = 10
+// media = (nota1 + nota2 + nota3 + nota4) / 4
+// console.log(media);
+
+// 'use strict'
+// let valor, num1, num2;
+// num1 = 10
+// num2 = 20
+// // valor = num1 > num2
+// // valor = num1 >= num2
+// // valor = num1 < num2
+// // valor = num1 == num2
+// // valor = num1 === num2
+
+// valor = num1 <= num2
+// console.log(valor);
+
+// let num = 5;
+// let num1 = 10;
+// let nome = "Jeferson";
+// console.log(typeof num);
+// console.log(num + num1)
+// console.log(nome.length);
+
+// let media = 7
+// let faltas = 5
+// let res = media >= 7 && faltas <= 5 ? "Aprovado" : "Reprovado"
+// console.log(res);
+
+let mensagem = alert (`Olá, Mundo!`) 
+let nome = prompt(`Qual o seu nome?`)
+alert(`Boas-Vindas  ${nome} `)
+
+let idade, idade
+idade = 18
+idade = idade2 >= idade
+alert(idade)
---
+++ JS/JS03-Metodos/array.js
@@ -0,0 +1,45 @@
+// let grupo04 = ["Jeferson", "Gabriella", "Daniela", "Sarbina"]
+
+// console.log(grupo04[2]);
+// console.log(typeof grupo04[2]);
+// console.log(grupo04[3]);
+// console.log(typeof grupo04[3]);
+// console.log(grupo04[0]);
+// console.log(typeof grupo04[0]);
+
+// let musica = ["Birds of the father", "L'amour de mavie", "Chihiro", "Skinny", "Blue"]
+
+// console.log(musica[4]);
+// console.log(musica[3]);
+
+// let cupons = 'Desc10'
+
+// let menor = cupons.toLowerCase()
+// console.log(menor);
+
+// let maior = cupons.toUpperCase()
+// console.log(maior);,
+
+
+// let nome = prompt(`Qual seu nome?`)
+// let idade = prompt(`Qual sua idade?`)
+// let Bairro = prompt(`Bairro onde?`)
+
+// let maiusculo = nome.toUpperCase()
+// console.log(maiusculo);
+
+// let 
+
+
+// Substring e Slipt
+
+// let navegador = 'Brave';
+// let novaString = navegador.substring(2)
+
+// console.log(novaString);
+
+
+let frase = 'Na moral mano!'
+console.log(frase);
+let novaF = frase.split(" ")
+console.log(novaF);
