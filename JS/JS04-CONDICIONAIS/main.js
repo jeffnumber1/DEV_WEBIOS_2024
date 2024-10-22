7492f53ae51aba1d31f41edab5bdf4d9bfe010dc
jeffnumber1 <jefersonfilho440@gmail.com>
2024-10-21 21:42:22 -0300

    js04

---
+++ JS/JS04-CONDICIONAIS/main.js
@@ -0,0 +1,33 @@
+// verificar se o caio está com a blusa do IOS
+
+let CaioComBlusa = true
+
+if(CaioComBlusa == true){
+    console.log("Caio pode assistir a aula")
+}else{
+    console.log("Caio você não vai assistir a aula e terá uma conversa com a mãe da Gabriella")
+}
+
+console.clear()
+
+// Um jogador somente irá passar para a fase 2 se atingir 1000 pontos
+
+let pontos = 1000
+
+if(pontos >= 1000){ // true
+    console.log("Passou para a fase 2")
+}
+
+// false
+
+console.log()
+
+// se o resto da divisao por 2 for igual a 0 é Par, mas se for 1 é Impar
+
+let imparPar = 18
+
+if(imparPar % 2 == 0){ //true
+    console.log("Par")
+}else{ //false
+    console.log("Impar")
+}
